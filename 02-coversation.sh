16bcfd7857d263b96f523f09f641e0f643bd923c
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-01-28 13:42:20 +0530

    actions

---
+++ 02-coversation.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+
+echo "suresh:hello ramesh, how are you?"
+echo "ramesh: hi suresh, Iam fine thans for asking.how are you doing?"
+echo "suresh: iam also fine.what are you doing these days?"
+echo "ramesh: iam learning shell scripting these days."
