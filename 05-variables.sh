69ceb662dbfedc6a21b5c1e6f5625b15d833c709
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-01-28 14:16:09 +0530

    actions

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,3 +3,6 @@
 echo  "please enter your username:"
 read USER_NAME
 echo "user name is $USER_NAME"
+
+echo "please enter your password:"
+read -s PASSWORD # -s option is used to hide the input while typing (password)
