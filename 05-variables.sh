6f4cc9311a3c0caaa1b3905af6bbdfa219cbbd34
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-01-28 14:20:57 +0530

    actions

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,3 +6,5 @@ echo "user name is $USER_NAME"
 
 echo "please enter your password:"
 read -s PASSWORD # -s option is used to hide the input while typing (password)
+
+echo "password is $PASSWORD" # in real time we should not print password like this.this is just only for testing purpose.
