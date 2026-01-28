12b227dd6fb897ace5c1d27ed3ea39bb24744f9e
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-01-28 14:36:29 +0530

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -6,5 +6,5 @@ echo "script executed at: $START_TIME"
 sleep 10 # sleep for 10 seconds to simulate some work being done
 
 END_TIME=$(date +%s)
-TOTAL_TIME=(($END_TIME - $START_TIME))
+TOTAL_TIME=$(($END_TIME - $START_TIME))
 echo  "script executed at: $TOTAL_TIME"
