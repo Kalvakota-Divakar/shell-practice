c8298031d22fceaea143e84abc540a3d969b7510
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-01-28 14:05:49 +0530

    actions

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash
+
+PERSON1=$1  #no space between variable name, equal sign and value
+PERSON2=$2
+echo "$PERSON1:hello $PERSON2, how are you?"
+echo "$PERSON2: hi $PERSON1, Iam fine thans for asking.how are you doing?"
+echo "$PERSON1: iam also fine.what are you doing these days?"
+echo "$PERSON2: iam learning shell scripting these days."
