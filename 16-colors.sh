747f3d75c07bf2ab5f614e200f2ba7ba18613f46
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-01-29 22:25:38 +0530

    actions

---
+++ 16-colors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# define color codes.
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e "$R Hello World $N, $Y I am learning Linux $N" # -e used to enable backslash escape sequences.
