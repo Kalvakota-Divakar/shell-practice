fb0d41597cea5c8dc44cb8c589a10b427794c189
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-02-02 21:05:48 +0530

    actions

---
+++ 21-script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#writing on script with another script calling it with source command.
+COUNTRY=INDIA
+
+echo "I am in: $COUNTRY"
+echo "PID of SCRIPT-1: $$"
+
+#sh 22-script2.sh # this will create a new subshell and PID will be different.
+source ./22-script2.sh
---
+++ 22-script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "I am in: $COUNTRY"
+echo "PID of SCRIPT-2: $$" # this will  be same as SCRIPT1 because we are sourcing this script.
