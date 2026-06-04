3df516f06d2c793eb348950b0257602aab4b1af5
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-06-04 12:17:25 +0530

    commit

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo  "please enter your username:"
-read USER_NAME
-echo "user name is $USER_NAME"
-
-echo "please enter your password:"
-read -s PASSWORD # -s option is used to hide the input while typing (password)
-
-echo "password is $PASSWORD" # in real time we should not print password like this.this is just only for testing purpose.
+echo "Enter your Username: "
+read  USERNAME
+echo "Enter password: "
