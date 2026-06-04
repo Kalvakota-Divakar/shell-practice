791eae6e71065d2223e0029171a0f08914e88f3d
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-06-04 12:10:29 +0530

    commit

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
 
-PERSON1=harsha #no space between variable name, equal sign and value
-PERSON2=naveen
-echo "$PERSON1:hello $PERSON2, how are you?"
-echo "$PERSON2: hi $PERSON1, Iam fine thans for asking.how are you doing?"
-echo "$PERSON1: iam also fine.what are you doing these days?"
-echo "$PERSON2: iam learning shell scripting these days."
+person1="suresh"
+person2="ramesh"
+echo "$person1:hello $person2, how are you?"
+echo "$person2: hi $person1, Iam fine thans for asking.how are you doing?"
+echo "$person1: iam also fine.what are you doing these days?"
+echo "$person2: iam learning shell scripting these days."
