90d923ec12e0d80b956f74c556c8ad685978d210
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-06-04 11:57:44 +0530

    commit

--- 02-coversation.sh
+++ 02-coversation.sh
@@ -1,7 +1,6 @@
  #!/bin/bash
-person1="suresh"
-person2="ramesh"
-echo "$person1:hello $person2, how are you?"
-echo "$person2: hi $person1, Iam fine thans for asking.how are you doing?"
-echo "$person1: iam also fine.what are you doing these days?"
-echo "$person2: iam learning shell scripting these days."
+
+echo "suresh:hello ramesh, how are you?"
+echo "ramesh: hi suresh, Iam fine thans for asking.how are you doing?"
+echo "suresh: iam also fine.what are you doing these days?"
+echo "ramesh: iam learning shell scripting these days."
