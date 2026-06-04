4038ae917ed75e5fd78c1bd710b53b21d79dc68c
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-06-04 11:48:08 +0530

    commit

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -2,6 +2,5 @@
 
 # this is the comment line,this line will not be executed by bash. This is only for information purpose.
 
-echo "Helloworld!"
-
-echo  "Hi, Iam learning Git"
+echo "Hello World"
+echo " iam learning shell"
