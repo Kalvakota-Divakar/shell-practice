cc27a974ddc5b78930cc64c87fa625bdfd70faf9
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-06-04 12:23:09 +0530

    commit

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-START_TIME=$(date +%s)
 
-echo "script executed at: $START_TIME"
+TIMESTAMP=$(date)
 
-sleep 10 # sleep for 10 seconds to simulate some work being done
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME - $START_TIME))
-echo  "script executed at: $TOTAL_TIME"
+echo "script executed at $TIMESTAMP"
